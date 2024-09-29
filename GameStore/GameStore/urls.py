4f70049a23c2a30d29981dfd62f6afe136d8a549
Alexey <alexey_kuzminykh@mail.ru>
2024-09-29 10:48:50 +0300

    MVT. Вывод объектов в шаблоны

--- GameStore/GameStore/urls.py
+++ GameStore/GameStore/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from task1 import views  # Импорт из приложения task1
+from task1.views import sign_up_by_django
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('platform/', views.main_page, name='main_page'),
+    path('games/', views.shop_page, name='shop_page'),
+    path('cart/', views.cart_page, name='cart_page'),
+    path('signup/', sign_up_by_django, name='signup'),
 ]
+
--- GameStore/task1/views.py
+++ GameStore/task1/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from .forms import UserRegister
+from .models import Buyer
+from .models import Game
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'fourth_task/platform.html')
+
+
+def shop_page(request):
+    games = Game.objects.all()
+    return render(request, 'fourth_task/games.html', {'games': games})
+
+
+def cart_page(request):
+    return render(request, 'fourth_task/cart.html')
+
+
+users = ['testuser']
+
+
+def sign_up_by_django(request):
+    info = {}
+
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            # Проверяем, существует ли пользователь с таким именем в таблице Buyer
+            if Buyer.objects.filter(name=username).exists():
+                info['error'] = 'Пользователь с таким именем уже существует'
+            elif password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            else:
+                # Добавляем нового покупателя в таблицу Buyer
+                Buyer.objects.create(name=username, balance=0, age=age)
+                return render(request, 'fifth_task/registration_page.html', {'message': f'Приветствуем, {username}!'})
+
+    else:
+        form = UserRegister()
+
+    return render(request, 'fifth_task/registration_page.html', {'form': form, 'error': info.get('error', '')})
